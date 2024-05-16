dd32281f3c080acdabbdb965a0ed667eb383bcaa
Przemysław Kozielski <Przemyslaw.Kozielski@kyndryl.com>
2024-05-16 11:09:38 +0200

    lab1 updated

--- lab1/main.tf
+++ lab1/main.tf
@@ -1,2 +1,18 @@
 #test
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.103.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  skip_provider_registration = true
+    features {}
+}
+
+
