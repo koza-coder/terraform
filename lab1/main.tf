2cd8585c6f8fe1bcaaf20056bf7751548367b29f
Przemysław Kozielski <Przemyslaw.Kozielski@kyndryl.com>
2024-05-17 10:36:49 +0200

    added lab2

--- lab1/main.tf
+++ lab1/main.tf
@@ -23,15 +23,3 @@ resource "azurerm_resource_group" "example_rg" {
   }
 }
 
-resource "azurerm_storage_account" "example_storage" {
-  name                     = "mystorageaccount12334455"
-  resource_group_name      = "testowa_rg"
-  location                 = "East US"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-
-  tags = {
-    environment = "staging"
-  }
-}
