cec69456e6fab1325fb6b51c8e439d7d42ee344b
Przemysław Kozielski <Przemyslaw.Kozielski@kyndryl.com>
2024-05-29 01:51:38 +0200

    lab 11 added, virtual machines

--- 11_virtual_machine/main.tf
+++ 11_virtual_machine/main.tf
@@ -77,6 +77,7 @@ resource "azurerm_network_interface" "example-interface" {
 
 output "subnets" {
   value = azurerm_virtual_network.example-vn.subnet
+  depends_on = [ azurerm_subnet.subnet1, azurerm_subnet.subnet2 ]
   
 }
 
@@ -88,4 +89,58 @@ resource "azurerm_public_ip" "example-public-ip" {
   allocation_method   = "Static"
 }
 
+resource "azurerm_network_security_group" "nsg-testowa1" {
+  name                = "nsg-testowa1"
+  location            = azurerm_resource_group.example_rg.location
+  resource_group_name = azurerm_resource_group.example_rg.name
 
+  security_rule {
+    name                       = "allowRDPRule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "association-nsg-testowa1" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg-testowa1.id
+}
+
+
+resource "azurerm_windows_virtual_machine" "vm-koza" {
+  name                = "vm-koza"
+  resource_group_name = azurerm_resource_group.example_rg.name
+  location            = azurerm_resource_group.example_rg.location
+  size                = "Standard_F2"
+  admin_username      = "abcd"
+  # WARNING: This is an example password and is not secure  !!!!!!!!
+  #  !!!!!!!!
+  admin_password      = "abcdefg"
+  # WARNING: This is an example password and is not secure  !!!!!!!!
+
+  network_interface_ids = [
+    azurerm_network_interface.example-interface.id ,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
