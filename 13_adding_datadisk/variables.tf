0c9080da5d8730ed2062c0472d9fb390cada507c
Przemysław Kozielski <Przemyslaw.Kozielski@kyndryl.com>
2024-05-29 03:03:18 +0200

    lab13 added, adding datadisk

---
+++ 13_adding_datadisk/variables.tf
@@ -0,0 +1,9 @@
+
+variable "password" {
+  description = "The password for the Windows VM"
+  type        = string
+  sensitive   = true
+  # WARNING: This is an example password and is not secure  !!!!!!!!
+  #default     = "abcdefg"
+  # WARNING: This is an example password and is not secure  !!!!!!!!
+}
